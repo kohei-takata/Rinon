d50668d133d522423e91c4ca72def9db27a3b284
Kohei TAKATA <kt.koheitakata@gmail.com>
2016-03-15 19:09:53 +0900

    Change to use English

--- src/components/UrlBarComponent.jsx
+++ src/components/UrlBarComponent.jsx
@@ -6,7 +6,7 @@ export default class UrlBarComponent extends React.Component {
         super(props);
     }
 
-    back() {
+    back(e) {
         document.getElementById(`${this.props.main.name}-webview`).goBack();
     }
 
@@ -35,9 +35,9 @@ export default class UrlBarComponent extends React.Component {
         return (
             <div className="pure-menu pure-menu-horizontal" id="urlBar">
                 <ul className="pure-menu-list">
-                    <li className="pure-menu-item"><a href="#" className="pure-menu-link" onClick={this.back.bind(this)}>戻る</a></li>
-                    <li className="pure-menu-item"><a href="#" className="pure-menu-link" onClick={this.forward.bind(this)}>進む</a></li>
-                    <li className="pure-menu-item"><a href="#" className="pure-menu-link" onClick={this.reload.bind(this)}>更新</a></li>
+                    <li className="pure-menu-item"><a href="#" className="pure-menu-link" onClick={this.back.bind(this)}>Back</a></li>
+                    <li className="pure-menu-item"><a href="#" className="pure-menu-link" onClick={this.forward.bind(this)}>Forward</a></li>
+                    <li className="pure-menu-item"><a href="#" className="pure-menu-link" onClick={this.reload.bind(this)}>Update</a></li>
                     <li className="pure-menu-item"><a href="#" className="pure-menu-link" onClick={this.openDevTool.bind(this)}>DevTool</a></li>
                     <li className="pure-menu-item"><input type="url" id="url" value={this.props.url} onChange={this._handleChangeUrl.bind(this)} /></li>
                 </ul>
